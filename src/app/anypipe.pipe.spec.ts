a1d6974e38c78c6cbe0cb09cc37756ab1b997a0f
Mohamed1995Ahmed <engmohamedabdelhakem@gmail.com>
2024-11-20 19:40:15 +0200

    addf

---
+++ src/app/anypipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AnypipePipe } from './anypipe.pipe';
+
+describe('AnypipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new AnypipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
